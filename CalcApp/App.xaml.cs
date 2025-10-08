8072a495e036be8333c172f0af97b9663cb04a35
norbexgang <norbi.cserpnyak@gmail.com>
2025-10-08 08:31:01 +0200

    Refactor animation easing functions in App.xaml and remove unused properties in App.xaml.cs for improved clarity and performance.

--- CalcApp/App.xaml.cs
+++ CalcApp/App.xaml.cs
@@ -1,10 +1,7 @@
 using System.Windows;
-using System.Windows.Media.Animation;
 
 namespace CalcApp;
 
 public partial class App : Application
 {
-    public static readonly QuadraticEase EaseOut = new() { EasingMode = EasingMode.EaseOut };
-    public static readonly QuadraticEase EaseIn = new() { EasingMode = EasingMode.EaseIn };
 }
