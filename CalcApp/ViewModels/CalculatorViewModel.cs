5668b3d533116edaf336f2384e03a83f258c42e0
Norbexgang <norbi.cserpnyak@gmail.com>
2025-12-01 15:08:31 +0100

    cleanup: remove unused namespaces from CalculatorViewModel

--- CalcApp/ViewModels/CalculatorViewModel.cs
+++ CalcApp/ViewModels/CalculatorViewModel.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Globalization;
-using System.IO;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows.Input;
